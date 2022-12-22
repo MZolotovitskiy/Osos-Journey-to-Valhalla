9092a5002c958d252c69ca97a0f44c58d93f68f4
Mikhail Zolotovitskiy <junior@zolotovitskiy.ru>
2022-12-22 20:36:22 +0300

    Added functions.py and Data

---
+++ functions.py
@@ -0,0 +1,25 @@
+import pygame
+import os
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def start_screen():
+    intro_text = []
+    intro_background = None
+    font = None
