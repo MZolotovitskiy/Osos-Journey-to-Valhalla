a6d3a3b4557187797ff5e1f23f7552b0ead7c52f
“YValentin <nbevqntb@gmail.com>
2022-12-22 20:33:10 +0300

    Added background music (Signs of Life)

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('aboba')
+import pygame
+
+pygame.init()
+size = WIDTH, HEIGHT = 1280, 720
+screen = pygame.display.set_mode(size)
+
+pygame.mixer.init()
+music = pygame.mixer.Sound('data/menu.mp3')
+music.play()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
