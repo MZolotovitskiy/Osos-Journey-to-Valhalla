fb45241afff8e9257afc99463b63db65cd0f8a58
WYud1n <bevqntb@gmail.com>
2023-01-08 16:58:21 +0300

    new branch '1stLevel'

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import pygame
-from os import environ
-
-environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
-
-
-
-pygame.init()
-size = WIDTH, HEIGHT = 1280, 720
-screen = pygame.display.set_mode(size)
-
-pygame.mixer.init()
-music = pygame.mixer.Sound('data/music/menuLoop.mp3')
-music.play()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+print('NEW BRANCH YEEEE')
