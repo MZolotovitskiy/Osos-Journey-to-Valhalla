b82c59f9efca9b16965522bdf5df1774477857f7
Mikhail Zolotovitskiy <junior@zolotovitskiy.ru>
2023-01-18 23:09:21 +0300

    Made start screen and 1st level work together

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from functions import start_screen
 import sys
 import pygame
 from os import environ
 
+file_name = 'Data/levels/Svartalfh3im.txt'
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
-
 
 FPS = 60
 pygame.init()
@@ -15,6 +14,28 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
 pygame.mixer.init()
+
+import pygame.draw
+
+FPS = 60
+
+
+def load_image(name, color_key=None):
+    try:
+        image = pygame.image.load(name).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
 osos_images = {'left': load_image('data/textures/mobs/osos/left.png', -1),
                'right': load_image('data/textures/mobs/osos/right.png', -1)}
 tile_images = {'wall': load_image('data/textures/blocks/obsidian.png'),
@@ -48,6 +69,184 @@ item_group = pygame.sprite.Group()
 interface_group = pygame.sprite.Group()
 projectiles = pygame.sprite.Group()
 mob_group = pygame.sprite.Group()
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def show_pos(pos):
+    pass
+
+
+def start_screen(screen):
+    buttons = [pygame.Rect((60, 340), (307, 87)), pygame.Rect((60, 427), (307, 87)), pygame.Rect((60, 514), (307, 87)),
+               pygame.Rect((60, 601), (307, 87))]
+    write_intro(screen, buttons)
+    pygame.display.flip()
+    menu_running = True
+    clock = pygame.time.Clock()
+    is_drawn = False
+    while menu_running:
+        mouse_pos = pygame.mouse.get_pos()
+        for i in range(len(buttons)):
+            if buttons[i].collidepoint(mouse_pos):
+                is_drawn = True
+                screen.fill('black')
+                write_intro(screen, buttons)
+                s = pygame.Surface((buttons[i][2], buttons[i][3]), pygame.SRCALPHA)
+                s.fill((190, 190, 190, 130))
+                screen.blit(s, (buttons[i][0], buttons[i][1] - 10))
+                break
+            elif is_drawn:
+                is_drawn = False
+                screen.fill('black')
+                write_intro(screen, buttons)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menu_running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if buttons[0].collidepoint(mouse_pos):
+                    return
+                if buttons[2].collidepoint(mouse_pos):
+                    settings(screen)
+                if buttons[3].collidepoint(mouse_pos):
+                    terminate()
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def settings(screen):
+    volume_bar_coord_x = (pygame.mixer.music.get_volume() * 720) + 60
+    volume_bar = pygame.Rect((60, 437), (720, 10))
+    sound_square = pygame.Rect((500, 482), (30, 30))
+    return_buttton = pygame.Rect(60, 544, 481, 67)
+    if pygame.mixer.music.get_busy():
+        sound_enabled = True
+    else:
+        sound_enabled = False
+    write_settings(screen, volume_bar, volume_bar_coord_x, sound_square, sound_enabled)
+    pygame.display.flip()
+    clock = pygame.time.Clock()
+    settings_running = True
+    while settings_running:
+        write_settings(screen, volume_bar, volume_bar_coord_x, sound_square, sound_enabled)
+        mouse_pos = pygame.mouse.get_pos()
+        if return_buttton.collidepoint(mouse_pos):
+            s = pygame.Surface((return_buttton[2], return_buttton[3]), pygame.SRCALPHA)
+            s.fill((190, 190, 190, 130))
+            screen.blit(s, (return_buttton[0], return_buttton[1]))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if volume_bar.collidepoint(mouse_pos):
+                    volume_bar_coord_x = mouse_pos[0]
+                    pygame.mixer.music.set_volume(1 * (mouse_pos[0] - 60) / 720)
+                if sound_square.collidepoint(mouse_pos):
+                    sound_enabled = not sound_enabled
+                    if not sound_enabled:
+                        pygame.mixer.music.stop()
+                    else:
+                        pygame.mixer.music.play()
+                if return_buttton.collidepoint(mouse_pos):
+                    return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def write_intro(screen, buttons):
+    text_coord = 330
+    font = pygame.font.Font('C:/Windows/Fonts/times.ttf', 80)
+    name_text = 'Путешествие Ососа в Вальгаллу'
+    buttons_text = ['Новая игра', 'Сохранения', 'Настройки', 'Выход']
+    intro_bground = load_image('Data/Images/StartScreenBG.jpeg')
+    screen.blit(intro_bground, (0, 0))
+    name_rendered = font.render(name_text, True, pygame.Color('white'))
+    intro_rect = name_rendered.get_rect()
+    intro_rect.x += 40
+    intro_rect.y += 30
+    screen.blit(name_rendered, intro_rect)
+    font = pygame.font.Font('C:/Windows/Fonts/times.ttf', 60)
+    for line in buttons_text:
+        string_rendered = font.render(line, True, pygame.Color('white'))
+        intro_size = string_rendered.get_rect()
+        intro_rect = intro_size
+        intro_rect.x = 60
+        text_coord += 20
+        intro_rect.top = text_coord
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+
+def write_settings(screen, volume_bar, volume_bar_coord_x, sound_square, sound_enabled):
+    screen.fill('black')
+    intro_bground = load_image('Data/Images/StartScreenBG.jpeg')
+    screen.blit(intro_bground, (0, 0))
+    text_coord = 330
+    font = pygame.font.Font('C:/Windows/Fonts/times.ttf', 80)
+    name_text = 'Путешествие Ососа в Вальгаллу'
+    settings_text = 'Настройки'
+    name_rendered = font.render(name_text, True, pygame.Color('white'))
+    intro_rect = name_rendered.get_rect()
+    intro_rect.x += 40
+    intro_rect.y += 30
+    screen.blit(name_rendered, intro_rect)
+    name_rendered = font.render(settings_text, True, pygame.Color('white'))
+    intro_rect = name_rendered.get_rect()
+    intro_rect.x += 40
+    intro_rect.y += 260
+    screen.blit(name_rendered, intro_rect)
+    font = pygame.font.Font('C:/Windows/Fonts/times.ttf', 60)
+    volume_text = 'Громкость'
+    string_rendered = font.render(volume_text, True, pygame.Color('white'))
+    intro_size = string_rendered.get_rect()
+    intro_rect = intro_size
+    intro_rect.x = 60
+    text_coord += 20
+    intro_rect.top = text_coord
+    text_coord += intro_rect.height
+    screen.blit(string_rendered, intro_rect)
+    text_coord += 20
+    pygame.draw.rect(screen, 'white', volume_bar)
+    pygame.draw.circle(screen, (130, 130, 130), (volume_bar_coord_x, text_coord + 5), 10)
+    string = 'Выключить звук'
+    string_rendered = font.render(string, True, pygame.Color('white'))
+    intro_size = string_rendered.get_rect()
+    intro_rect = intro_size
+    intro_rect.x = 60
+    text_coord += 20
+    intro_rect.top = text_coord
+    text_coord += intro_rect.height
+    screen.blit(string_rendered, intro_rect)
+    if sound_enabled:
+        pygame.draw.rect(screen, 'white', sound_square, 1)
+    else:
+        pygame.draw.rect(screen, 'white', sound_square)
+    string = 'Вернуться в меню'
+    string_rendered = font.render(string, True, pygame.Color('white'))
+    string_size = string_rendered.get_rect()
+    string_rect = intro_size
+    string_rect.x = 60
+    text_coord += 20
+    string_rect.top = text_coord
+    screen.blit(string_rendered, string_rect)
+
+
+def load_level(filename):
+    # filename = "data/" + filename
+    try:
+        with open(filename, 'r') as mapFile:
+            level_map = [line.strip() for line in mapFile]
+        max_width = max(map(len, level_map))
+        return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+    except FileNotFoundError as f:
+        print(f'Файл не найден')
+        exit(-1)
+
+
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
@@ -225,8 +424,44 @@ class Camera:
         self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
 
 
-music = pygame.mixer.Sound('data/music/menuLoop.mp3')
-music.play()
+def generate_level(level):
+    global Key
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+            elif level[y][x] == 'P':
+                Tile('portal', x, y)
+            elif level[y][x] == 'M':
+                Tile('empty', x, y)
+                Mob(x, y, 'scarecrow', 5)
+            elif level[y][x] == 'G':
+                Tile('empty', x, y)
+                Gid(x, y, 'gid', 100)
+            elif level[y][x] == 'K':
+                Tile('empty', x, y)
+                Key = AnimatedSprite(key_image, 16, 1, x, y)
+    return new_player, x, y
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def interface_init():
+    pass
+
+
+pygame.mixer.music.load('Data/music/01_menuLoop.mp3')
+pygame.mixer.music.play(loops=-1)
+start_screen(screen)
 pygame.mixer.init()
 pygame.mixer.music.load('data/music/02_Svartalfheim.mp3')
 pygame.mixer.music.play(loops=-1)
@@ -286,4 +521,3 @@ while running:
     pygame.display.flip()
     clock.tick(FPS)
 terminate()
-
